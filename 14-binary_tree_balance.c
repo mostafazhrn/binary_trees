c7dfb83a2e1c7ed6f45644ec1b90d7b37795149d
mostafazhrn <mostafazhrn@gmail.com>
2024-01-30 11:58:41 -0500

    tree_balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,54 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - This code shall measure the height of a binary tree
+ * @tree: This shall represent the root node of the tree to measure the height
+ * Return: This shall return the height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t lft_hght = 0;
+size_t rt_hght = 0;
+if (tree == NULL)
+{
+return (0);
+}
+if (tree->left != NULL)
+{
+lft_hght = binary_tree_height(tree->left) + 1;
+}
+if (tree->right != NULL)
+{
+rt_hght = binary_tree_height(tree->right) + 1;
+}
+if (lft_hght > rt_hght)
+{
+return (lft_hght);
+}
+else
+{
+return (rt_hght);
+}
+}
+/**
+ * binary_tree_balance - This code shall measure the balance factor of tree
+ * @tree: This shall represent the root node of the tree to measure the balance
+ * Return: This shall return the balance factor of the tree
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+int lft_hght = 0;
+int rt_hght = 0;
+if (tree == NULL)
+{
+return (0);
+}
+if (tree->left != NULL)
+{
+lft_hght = binary_tree_height(tree->left) + 1;
+}
+if (tree->right != NULL)
+{
+rt_hght = binary_tree_height(tree->right) + 1;
+}
+return (lft_hght - rt_hght);
+}
